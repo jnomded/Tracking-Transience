ac657dcb306e1898f7ced619a392766e759a07c5
jnomded <james.edmond04@gmail.com>
2024-08-26 18:17:29 -0500

    Initial Commit

---
+++ Tracking Transience/Tracking_TransienceApp.swift
@@ -0,0 +1,32 @@
+//
+//  Tracking_TransienceApp.swift
+//  Tracking Transience
+//
+//  Created by James Edmond on 8/26/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Tracking_TransienceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
